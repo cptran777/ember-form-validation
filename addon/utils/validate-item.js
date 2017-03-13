fd534b8fbf3e3f37b9eca6b08eeafda2800388e6
Charlie Tran <catran@linkedin.com>
2017-03-13 15:25:37 -0700

    backbone for validate item function

--- addon/utils/validate-item.js
+++ addon/utils/validate-item.js
@@ -1,3 +1,6 @@
+import addError from 'ember-form-validation/utils/errors';
+import formatTest from 'ember-form-validation/utils/format';
+
 /*
  * The validateItem function is used in the form-validation mixin to handle the
  * validation for a single item. Thus, this is also the main entry point for all
@@ -16,9 +19,42 @@
 export default function validateItem(item, value, criteria, errors) {
   let format, customFormat;
 
+  // First check to see if the item is required, and if so it has a valid value
   if (criteria.required &&
      (!value || (typeof value === 'string' && value.length === 0))) {
-    return;
+    return addError(item, 'required', criteria, errors);
+  }
+
+  // Priority check for a custom format, otherwise check for a regular format
+  customFormat = criteria.customFormat;
+  format = customFormat ? 'custom' : criteria.format;
+
+  if (format && !formatTest(format, value, customFormat)) {
+    return addError(item,
+                    format === 'custom' ? 'customFormat' : 'format',
+                    criteria,
+                    errors);
+  }
+
+  // Setup additional tests if necessary
+  switch (format) {
+    case 'word':
+    case 'words':
+    case 'fullname':
+      break;
+
+    case 'number':
+      break;
+
+    case 'date-MMYYYY':
+    case 'date-MMDDYYYY':
+      break;
+
+    case 'custom':
+      break;
+
+    default:
+      // Hmm...
   }
 
   return true;
